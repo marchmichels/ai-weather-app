6b7ebbdc34f0f41a20c874286768558abf4a21dc
marchmichels <56034198+marchmichels@users.noreply.github.com>
2023-05-08 16:02:52 -0400

    Created CityPicker component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
-import Image from 'next/image'
+'use client'
+
+import CityPicker from "@/components/CityPicker";
+import { Card, Divider, Subtitle, Text } from "@tremor/react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7E] p-10 flex flex-col justify-center">
 
-      <h1>GPT-4 Weather App</h1>
+      <Card className="max-w-4xl mx-auto">
+        <Text className="text-6xl font-bold text-center mb-10">
+          Weather AI
+        </Text>
+        <Subtitle className="text-xl text-center">
+          Powered by OpenAI, Next.js 13.4.1, Tailwind Css, Tremor 2.0 + More!
+        </Subtitle>
+
+        <Divider className="my-10" />
+
+        <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7E]">
+            <CityPicker />
+        </Card>
+      </Card>
 
 
-    </main>
+
+    </div>
   )
 }
---
+++ components/CityPicker.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { Country, City } from "country-state-city";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Select from "react-select";
+import { GlobeIcon} from "@heroicons/react/solid";
+
+type option = {
+    value: {
+        latitude: string;
+        longitude: string;
+        isoCode: string;
+    };
+    label: string;
+} | null;
+
+type cityOption = {
+    value: {
+        latitude: string;
+        longitude: string;
+        countryCode: string;
+        name: string;
+        stateCode: string;
+    };
+    label: string;
+} | null;
+
+const options = Country.getAllCountries().map(country => ({
+    value: {
+        latitude: country.latitude,
+        longitude: country.longitude,
+        isoCode: country.isoCode,
+    },
+    label: country.name,
+}));
+
+function CityPicker() {
+
+    const [selectedCountry, setSelectedCountry] = useState<option>(null);
+    const [selectedCity, setSelectedCity] = useState<cityOption>(null);
+    const router = useRouter();
+
+    const handleSelectedCountry = (option: option) => {
+        setSelectedCountry(option);
+        setSelectedCity(null);
+    }
+
+    return (
+        <div className="space-y-4">
+            <div className="flex items-center space-x-2 text-white/80">
+                <GlobeIcon className="h-5 w-5 text-white" />
+                <label htmlFor="country">Country</label>
+            </div>
+            <Select
+                className="text-black"
+                value={selectedCountry}
+                onChange={handleSelectedCountry}
+                options={options}
+            />
+        </div>
+    );
+}
+
+export default CityPicker;
+
+
+
+
+
+//45:43 in video
