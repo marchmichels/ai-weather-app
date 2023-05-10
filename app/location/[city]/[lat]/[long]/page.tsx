648369e1ce6be2ec0e50948e15e53455a780b74f
marchmichels <56034198+marchmichels@users.noreply.github.com>
2023-05-10 14:38:24 -0400

    Create display dashboard

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -1,3 +1,8 @@
+import { getClient } from "@/apollo-client";
+import Calloutcard from "@/components/CalloutCard";
+import InformationPanel from "@/components/InformationPanel";
+import StatCard from "@/components/StatCard";
+import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
 
 type Props = {
     params: {
@@ -7,10 +12,101 @@ type Props = {
     }
 }
 
-function WeatherPage({params: {city, lat, long}}: Props) {
+async function WeatherPage({ params: { city, lat, long } }: Props) {
+
+    const client = getClient();
+
+    const { data } = await client.query({
+        query: fetchWeatherQuery,
+        variables: {
+            current_weather: "true",
+            longitude: long,
+            latitude: lat,
+            timezone: 'GMT'
+        }
+    })
+
+    const results: Root = data.myQuery;
+
+    //console.log(results);
+
     return (
         <div>
-            Welcome to the weather page: {city} {lat} {long}
+            <InformationPanel 
+            city={city} 
+            long={long}
+            lat={lat}
+            results={results} 
+            />
+            <div>
+                <div className="p-5">
+                    <div className="pb-5">
+                        <h2 className="text-xl font-bold">Todays Overview</h2>
+                        <p className="text-sm text-gray-400">
+                            Last Updated at: {" "}
+                            {new Date(results.current_weather.time).toLocaleString()}
+                            {results.timezone}
+                        </p>
+                    </div>
+
+                    <div className="m-2 mb-10">
+                        <Calloutcard
+                            message="This is where GPT Summary will go!"
+                        />
+                    </div>
+
+                    <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
+                        <StatCard
+                            title="Maximum Temperature"
+                            metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°`}
+                            color="yellow"
+                        />
+
+                        <StatCard
+                            title="Minimum Temperature"
+                            metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°`}
+                            color="green"
+                        />
+
+                        <div>
+                            <StatCard
+                                title="UV Index"
+                                metric={`${results.daily.uv_index_max[0].toFixed(1)}`}
+                                color="rose"
+                            />
+                            {Number(results.daily.uv_index_max[0].toFixed(1)) > 5 && (
+                                <Calloutcard
+                                    message={"The UV is high today, be sure to wear SPF!"}
+                                    warning
+                                />
+                            )}
+                        </div>
+
+                        <div className="flex space-x-3">
+                            <StatCard
+                                title="Wind Speed"
+                                metric={`${results.current_weather.windspeed.toFixed(1)}m/s`}
+                                color="cyan"
+                            />
+                            <StatCard
+                                title="Wind Direction"
+                                metric={`${results.current_weather.winddirection.toFixed(1)}°`}
+                                color="violet"
+                            />
+
+                        </div>
+                    </div>
+
+                </div>
+                <hr className="mb-5" />
+                <div className="space-y-3">
+                    {/* Temp chart */}
+                    {/* Rain chart */}
+                    {/* Humidity chart */}
+
+                </div>
+
+            </div>
         </div>
     );
 }
---
+++ lib/weatherCodeToString.ts
@@ -0,0 +1,121 @@
+const weatherCodeToString: {
+    [key: number]: {
+        icon: string;
+        label: string;
+    };
+} = {
+    0: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    1: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    2: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    3: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    45: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    48: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    51: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    53: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    55: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    56: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    57: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    61: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    63: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    65: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    66: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    67: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    71: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    73: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    75: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    77: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    80: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    81: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    82: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    85: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    86: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    95: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    96: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+    99: {
+        icon: "c01d",
+        label: "Clear sky",
+    },
+};
+
+export default weatherCodeToString;
